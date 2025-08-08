4858f1a21b5e2a1616fe7dbf1782ef6d66408f73
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-08 21:16:19 +0000

    agrega este logo al nombre de la app en todos los lugares que este el no

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -23,14 +23,22 @@ export const Icons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       {...props}
     >
       <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-      <path d="m9.5 14.5 5-5" />
-      <path d="m14.5 14.5-5-5" />
+      <g transform="rotate(45 12 12) translate(0, -1)">
+        <path d="M15.5 8.5h-7L7 10h8.5Z" />
+        <path d="M15.5 13.5h-7L7 12h8.5Z" />
+        <path d="M14 10v2" />
+      </g>
+       <g transform="rotate(-45 12 12) translate(0, -1)">
+        <path d="M15.5 8.5h-7L7 10h8.5Z" />
+        <path d="M15.5 13.5h-7L7 12h8.5Z" />
+        <path d="M14 10v2" />
+      </g>
     </svg>
   ),
 };
