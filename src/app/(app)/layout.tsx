59befdbf5ed3b2b2c7c54bc57a1c819077ded306
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-09 01:08:56 +0000

    el recuadro donde esta el nombre y el logo hazlo mas abajo ya qeu esta m

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -92,7 +92,7 @@ export default function DashboardLayout({
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-card text-card-foreground md:block">
-        <div className="flex h-full max-h-screen flex-col gap-6">
+        <div className="flex h-full max-h-screen flex-col gap-6 pt-6">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold text-primary">
               <Icons.logo className="h-8 w-8" />
@@ -156,7 +156,7 @@ export default function DashboardLayout({
                 <span className="sr-only">Toggle navigation menu</span>
               </Button>
             </SheetTrigger>
-            <SheetContent side="left" className="flex flex-col bg-card text-card-foreground">
+            <SheetContent side="left" className="flex flex-col bg-card text-card-foreground pt-6">
               <nav className="grid gap-6 text-lg font-medium">
                 <Link
                   href="/"
