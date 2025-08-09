b14a14f1b60e0ae09d6c30ba575e47e25c907edc
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-09 01:10:27 +0000

    el nombre y el logo hazlo que este centrado

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -92,16 +92,12 @@ export default function DashboardLayout({
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-card text-card-foreground md:block">
-        <div className="flex h-full max-h-screen flex-col gap-6 pt-6">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex h-full max-h-screen flex-col gap-8 pt-6">
+          <div className="flex h-14 items-center justify-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold text-primary">
               <Icons.logo className="h-8 w-8" />
               <span className="text-2xl">SquadUp</span>
             </Link>
-            <Button variant="ghost" size="icon" className="ml-auto h-8 w-8">
-              <Bell className="h-4 w-4" />
-              <span className="sr-only">Toggle notifications</span>
-            </Button>
           </div>
           <div className="flex-1">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
@@ -214,6 +210,10 @@ export default function DashboardLayout({
               </div>
             </form>
           </div>
+          <Button variant="ghost" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+          </Button>
           <UserNav />
         </header>
         <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8 bg-background pb-24 md:pb-8">
