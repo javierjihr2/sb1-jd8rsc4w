53d2d3781ee3f521c99495b3f519c8379022a22f
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-09 01:06:31 +0000

    agranda el titulo de la app y el espacio que separa con los items que se

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -92,11 +92,11 @@ export default function DashboardLayout({
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-card text-card-foreground md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-full max-h-screen flex-col gap-4">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold text-primary">
               <Icons.logo className="h-8 w-8" />
-              <span className="">SquadUp</span>
+              <span className="text-xl">SquadUp</span>
             </Link>
             <Button variant="ghost" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
@@ -157,13 +157,13 @@ export default function DashboardLayout({
               </Button>
             </SheetTrigger>
             <SheetContent side="left" className="flex flex-col bg-card text-card-foreground">
-              <nav className="grid gap-2 text-lg font-medium">
+              <nav className="grid gap-4 text-lg font-medium">
                 <Link
                   href="/"
                   className="flex items-center gap-2 text-lg font-semibold text-primary mb-4"
                 >
                   <Icons.logo className="h-6 w-6" />
-                  <span>SquadUp</span>
+                  <span className="text-xl">SquadUp</span>
                 </Link>
                 {navItems.map((item) => (
                   <Link
