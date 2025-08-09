6ed2c247063aed0e5b47946ef3411fa103274025
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-09 02:38:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/feed/page.tsx
+++ src/app/(app)/feed/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card"
 import { Textarea } from "@/components/ui/textarea"
 import { feedPosts } from "@/lib/data"
 import { Heart, MessageCircle, Send, ImageIcon, Sticker, Rss } from "lucide-react"
