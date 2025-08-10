6c80f4611b296b564fbb588bafb9f8e019333aaf
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-10 02:55:55 +0000

    aun no noto cambios, hiciste lo que mencionaste?

--- src/app/(app)/chats/page.tsx
+++ src/app/(app)/chats/page.tsx
@@ -232,3 +232,4 @@ export default function ChatsPage() {
     </div>
   )
 }
+
--- src/components/chat-theme-settings.tsx
+++ src/components/chat-theme-settings.tsx
@@ -90,3 +90,4 @@ export function ChatThemeSettings({ children, setTheme, onUploadClick }: ChatThe
     </DropdownMenu>
   )
 }
+
