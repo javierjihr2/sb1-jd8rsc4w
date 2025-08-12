371d66266c3a2e64831652f4e448aad711a73a25
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-12 04:11:28 +0000

    me aparece esto: [error] Unable to launch Android Studio. Is it installe

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.squadup.app',
+  appName: 'SquadUp',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
