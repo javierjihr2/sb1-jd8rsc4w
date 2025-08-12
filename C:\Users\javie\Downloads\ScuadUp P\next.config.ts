78c8ccd498a8b0a6cdfbc214ac7d50fd678fab99
Javier Henríquez Rebolledo <javier.jihr2@gmail.com>
2025-08-12 05:26:10 +0000

    ME SIGUE APARECIENDO ESTO: [error] Could not find the web assets directo

---
+++ C:\Users\javie\Downloads\ScuadUp P\next.config.ts
@@ -0,0 +1,33 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'flagsapi.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // La siguiente línea es la solución. 
+  // Le dice a Next.js que ponga los archivos de la app en la carpeta "public".
+  distDir: 'public',
+};
+
+export default nextConfig;
